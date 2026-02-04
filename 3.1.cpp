8a362276613b8d421fc6f5fd01d097647d9fa0f3
ker4e <rus.shirokov.00@bk.ru>
2026-02-04 22:22:40 +0300

    Create 3.1.cpp

---
+++ 3.1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Address {
+private: 
+	std::string city{};
+	std::string street{};
+	int numberHouse{};
+	int numberApartment{};
+public:
+	Address() = default;
+	Address(std::string_view city, std::string_view street, int numberH, int numberAp) : 
+		city{city}, street{street}, numberHouse{numberH}, numberApartment{numberAp}
+	{ }
+
+	std::string outputAddress() const {
+		return city + ", " + street + ", " + std::to_string(numberHouse) + ", " + std::to_string(numberApartment);
+	}
+};
+
+int main() {
+	
+	std::ofstream ofile("in.txt");
+	if (ofile.is_open()) {
+		ofile << "4\n"
+			<< "Moscow\n" << "Builders\n" << "34\n" << "12\n"
+			<< "Omsk\n" << "Puskin\n" << "2\n" << "13\n"
+			<< "Rostov-on-Don\n" << "Voroshilovsky\n" << "43\n" << "56\n"
+			<< "Voronezh\n" << "Lenin\n" << "1\n" << "212\n";
+	}
+	ofile.close();
+
+	std::ifstream ifile("in.txt");
+	if (!ifile.is_open()) {
+		std::cout << "Error... in.txt is closed";
+		return 1;
+		}
+
+	int counter{};
+	ifile >> counter;
+	Address* addresses = new Address[counter];
+
+	for (int i{ counter - 1 }; i >= 0; --i) {
+		std::string city{};
+		std::string street{};
+		int numberHouse{};
+		int numberApartment{};
+
+		std::getline(ifile >> std::ws, city);
+		std::getline(ifile >> std::ws, street);
+		ifile >> numberHouse;
+		ifile >> numberApartment;
+
+		addresses[i] = Address(city, street, numberHouse, numberApartment);
+	}
+
+	ifile.close();
+
+	std::ofstream outFile("out.txt");
+	if (outFile.is_open()) {
+		outFile << counter << '\n';
+
+		for (int i{ 0 }; i < counter; ++i) {
+			outFile << addresses[i].outputAddress() << '\n';
+		}
+	}
+	outFile.close();
+	delete[] addresses;
+
+	return EXIT_SUCCESS;
+}
