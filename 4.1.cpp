14840cfb7f9dc533ef4711c1f513541c82905205
ker4e <rus.shirokov.00@bk.ru>
2026-02-19 21:42:21 +0300

    Update 4.1.cpp

--- 4.1.cpp
+++ 4.1.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
+#include <string>
 
 class Figure {
 protected:
     int sidesCount;
+    std::string name;
 
 public:
-    Figure(int sides) : sidesCount(sides) {}
+    Figure(int sides, const std::string& Name) : sidesCount(sides), name(Name) {}
 
     int getSidesCount() const {
         return sidesCount;
+    }
+
+    std::string getName() const {
+        return name;
     }
 };
 
 class Triangle : public Figure {
 public:
-    Triangle() : Figure(3) {}
+    Triangle() : Figure(3, "Triangle") {}
 };
 
-class  Quadrangle : public Figure {
+class Quadrangle : public Figure {
 public:
-    Quadrangle() : Figure(4) {}
+    Quadrangle() : Figure(4, "Quadrangle") {}
 };
 
 int main() {
-    Figure figure{ 0 };
+    Figure figure{ 0, "Figure" };
     Triangle triangle;
     Quadrangle quad;
 
-    std::cout << "Figure: " << figure.getSidesCount() << std::endl;
-    std::cout << "Triangle: " << triangle.getSidesCount() << std::endl;
-    std::cout << "Quadrangle: " << quad.getSidesCount() << std::endl;
+    std::cout << figure.getName() << ": " << figure.getSidesCount() << std::endl;
+    std::cout << triangle.getName() << ": " << triangle.getSidesCount() << std::endl;
+    std::cout << quad.getName() << ": " << quad.getSidesCount() << std::endl;
 
     return EXIT_SUCCESS;
 }
